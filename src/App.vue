4fdd3ec89bad86c256ca7ade8119f776a40b1470
Yana Rezantseva <yanarezantseva@yandex.ru>
2025-09-30 20:37:14 +0300

    init + structure

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <header></header>
+  <main class="main">
+    <div class="title-container">
+      <h1>Учетные записи</h1>
+    </div>
+    <div class="main-container">
+
+    </div>
+
+  </main>
+  <footer></footer>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: start;
+  height: 100vh;
+}
+.title-container {
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  gap: 20px;
+  width: 100%;
+  margin-bottom: 20px;
+}
+</style>
